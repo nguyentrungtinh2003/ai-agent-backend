66a9e53e919431602e892d64a28e6426b571da56
nguyentrungtinh <trungtinhn300@gmail.com>
2025-04-08 11:01:42 +0700

    Update file server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.config";
+import authRoutes from "./routes/auth.routes";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("api/", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
