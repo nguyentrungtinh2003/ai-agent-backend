b417a1325ac08c936356d09db51fd47ccd4be4a6
nguyentrungtinh <trungtinhn300@gmail.com>
2025-04-09 10:23:47 +0700

    Create structure write code AI to call

---
+++ services/ai.service.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const callDeepSeek = (prompt) => {
+  // Trung và Nghĩa viết hàm gọi lên API của deepseek và gửi kèm theo nội dung
+  // Dùng Axios để gọi API nha
+};
+
+const callGemini = (prompt) => {
+  // Trung và Nghĩa viết hàm gọi lên API của gemini và gửi kèm theo nội dung
+  // Dùng Axios để gọi API nha
+};
+
+export default {
+  callDeepSeek,
+  callDeepSeek,
+};
