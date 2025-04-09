e444efa40864ae0842df3f8dd1fb0652b96c3651
nguyentrungtinh <trungtinhn300@gmail.com>
2025-04-09 15:57:02 +0700

    Update file sendMail.js in utils

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587, // hoặc 587 nếu bạn muốn dùng TLS
+  secure: false, // true cho port 465, false cho 587,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
